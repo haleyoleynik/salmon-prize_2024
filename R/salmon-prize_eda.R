e10913369f224b47b45b0862f0f2422d4625e92a
haleyoleynik <h.oleynik@oceans.ubc.ca>
2024-05-22 14:42:55 -0700

    Update salmon-prize_eda.R

--- R/salmon-prize_eda.R
+++ R/salmon-prize_eda.R
@@ -32,14 +32,6 @@ long.df %>%
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   facet_grid(vars(System),scales = "free")
 
-# plot numbers over time for top 4 age classes  - can't see much 
-long.df %>%
-  filter(Number > 0) %>%
-  filter(Age %in% c("AgeClass_2.3", "AgeClass_1.2", "AgeClass_1.3", "AgeClass_2.2")) %>%
-  ggplot(aes(x = BroodYear, y = Number, color = Age)) +
-  geom_point(alpha = 0.2) +
-  geom_smooth(se = F, alpha = 0.5)
-
 # Calculate total returns 
 try <- df %>% 
   rowwise() %>%
